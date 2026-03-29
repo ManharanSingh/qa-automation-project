89fb54e121ee973e3e654b72ae84af0bcce0e3e6
ManharanSingh <manharanmaravi332@gmail.com>
2026-03-29 10:05:48 +0530

    Update test_checkout.py

--- project/tests/test_checkout.py
+++ project/tests/test_checkout.py
@@ -9,10 +9,8 @@ def test_checkout(driver_setup):
 
     login_page = LoginPage(driver)
     login_page.open("https://www.saucedemo.com/")
-    login_page.enter_username("standard_user")
-    login_page.enter_password("secret_sauce")
-    login_page.click_login()
-
+    login_page.login("standard_user", "secret_sauce")
+    
     inventory = InventoryPage(driver)
     inventory.add_to_cart()
     inventory.go_to_cart()
