41ab164b3de2d04a2793dad18c2007202eaa47bf
ManharanSingh <manharanmaravi332@gmail.com>
2026-03-30 01:51:27 +0530

    Update test_negative.py

--- api_testing/tests/test_negative.py
+++ api_testing/tests/test_negative.py
@@ -1,1 +1,21 @@
 
+
+
+
+class TestNegativeCases:
+
+    def test_get_nonexistent_post(self, client):
+        response = client.get("/posts/99999")
+
+        assert response.status_code == 404
+
+    def test_get_nonexistent_user(self, client):
+        response = client.get("/users/99999")  
+
+        assert response.status_code == 404
+
+    def test_response_time_is_acceptable(self, client):
+        response = client.get("/posts")
+
+        assert response.elapsed.total_seconds() < 3.0      
+        
