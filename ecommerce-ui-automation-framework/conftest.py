eced30582eb490ffe3f7779445b633aa555dd291
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-16 18:08:06 +0530

    Update conftest.py

--- ecommerce-ui-automation-framework/conftest.py
+++ ecommerce-ui-automation-framework/conftest.py
@@ -4,11 +4,22 @@ import os
 
 @pytest.fixture
 def driver_setup():
-   
-    driver = webdriver.Edge()    
-    yield driver
+   from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pytest
 
-    driver.quit()  
+@pytest.fixture
+def driver_setup():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+
+    yield driver
+    driver.quit()
 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
