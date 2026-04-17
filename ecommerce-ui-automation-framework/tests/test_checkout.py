cdc75d0122fbd2e22c3f78182742ce9bbfe2347f
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-17 17:14:22 +0530

    Update test_checkout.py

--- ecommerce-ui-automation-framework/tests/test_checkout.py
+++ ecommerce-ui-automation-framework/tests/test_checkout.py
@@ -18,27 +18,27 @@ def test_checkout(driver_setup):
     login_page.login(USERNAME, PASSWORD)
     
     inventory = InventoryPage(driver)
-    time.sleep(3)
+    
     logger.info("Adding item to cart")
     inventory.add_to_cart()
-    time.sleep(3)
+    
     logger.info("Navigating to cart page")
     inventory.go_to_cart()
-    time.sleep(3)
+    
     checkout = CheckoutPage(driver)
 
     logger.info("Navigating to checkout page")
     checkout.click_checkout()
-    time.sleep(3)
+    
     logger.info("Entering checkout details: first_name=Manharan, last_name=Maravi, zip=501401")
     checkout.fill_details(FIRST_NAME, LAST_NAME, ZIP_CODE)
-    time.sleep(3)
+    
     logger.info("Finishing checkout process")
     checkout.finish_order()
-    time.sleep(3)
+    
     success_msg = checkout.verify_success_message()
     logger.info(f"Order success message: {success_msg}")
-    time.sleep(3)
+    
     assert success_msg == "Thank you for your order!", \
     f"Expected success message not found. Got: {success_msg}"
    
