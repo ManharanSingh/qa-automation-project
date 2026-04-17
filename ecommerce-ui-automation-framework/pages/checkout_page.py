57cb8096abc5a26b481789f9b9e46a17c5a3ae9b
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-17 17:30:03 +0530

    Update checkout_page.py

--- ecommerce-ui-automation-framework/pages/checkout_page.py
+++ ecommerce-ui-automation-framework/pages/checkout_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from utils.base_page import BasePage
+from selenium.webdriver.support import expected_conditions as EC
 
 class CheckoutPage(BasePage):
     def __init__(self, driver):
