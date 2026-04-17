911f98f6cb3d9897714e441d1fbd9e39e47c4081
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-17 17:47:17 +0530

    Update checkout_page.py

--- ecommerce-ui-automation-framework/pages/checkout_page.py
+++ ecommerce-ui-automation-framework/pages/checkout_page.py
@@ -5,11 +5,11 @@ from selenium.webdriver.support import expected_conditions as EC
 class CheckoutPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
-        self.first_name_locator = (By.CSS_SELECTOR, "#first-name")
-        self.last_name_locator = (By.CSS_SELECTOR, "#last-name")
-        self.postal_code_locator = (By.CSS_SELECTOR, "#postal-code")
+        self.first_name_locator = (By.ID, "first-name")
+        self.last_name_locator = (By.ID, "last-name")
+        self.postal_code_locator = (By.ID, "#postal-code")
         self.checkout_btn = (By.ID, "checkout")
-        self.continue_btn = (By.CSS_SELECTOR, "#continue")
+        self.continue_btn = (By.ID, "continue")
         self.finish_btn = (By.ID, "finish")
         self.success_message = (By.TAG_NAME, "h2")
 
@@ -22,6 +22,7 @@ class CheckoutPage(BasePage):
         self.type(self.last_name_locator, last_name)
         self.type(self.postal_code_locator, zip_code)
         self.click(self.continue_btn)
+        self.wait.until(EC.url_contains("checkout-step-two"))
         
     def finish_order(self):
         self.click(self.finish_btn)
