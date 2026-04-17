e58d419da21fe579ba428f443001a7fffa6f0629
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-17 17:13:15 +0530

    Update test_cart.py

--- ecommerce-ui-automation-framework/tests/test_cart.py
+++ ecommerce-ui-automation-framework/tests/test_cart.py
@@ -2,7 +2,7 @@ from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 from config.config import BASE_URL, USERNAME, PASSWORD
 from utils.logger import get_logger
-import time
+
 
 logger = get_logger(__name__)
 
@@ -16,23 +16,23 @@ def test_cart(driver_setup ):
     login_page.login(USERNAME, PASSWORD)
     
     inventory = InventoryPage(driver)
-    time.sleep(3)
+    
     logger.info("Adding item to cart") 
     inventory.add_to_cart()
-    time.sleep(3)
+    
     logger.info("Navigating to cart page")
     inventory.go_to_cart()
-    time.sleep(3)
+    
     cart_count = inventory.get_cart_count()
     logger.info(f"Cart count after adding item: {cart_count}")
-    time.sleep(3)
+    
     assert inventory.get_cart_count() == 1, f"Expected cart count 1, but got {cart_count}"
-    time.sleep(3)
+    
     inventory.remove_item()
-    time.sleep(3)
+    
     updated_count = inventory.get_removed_items_count()
     logger.info(f"Cart count after removing item: {updated_count}")
-    time.sleep(3)
+    
     logger.info("verifying empty cart")
     assert inventory.get_removed_items_count() == 0, f"Expected empty cart, but got {updated_count}"
     
