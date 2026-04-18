4f8e549f6fae8a009bb0ee033fd305677add198a
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-18 14:06:29 +0530

    Update checkout_page.py

--- ecommerce-ui-automation-framework/pages/checkout_page.py
+++ ecommerce-ui-automation-framework/pages/checkout_page.py
@@ -15,7 +15,7 @@ class CheckoutPage(BasePage):
 
     def click_checkout(self):
         self.click(self.checkout_btn)
-        self.wait.until(EC.url_contains("checkout-step-one"))
+        self.wait.until(EC.visibility_of_element_located(self.continue_btn))
         
     def fill_details(self, name, last_name, zip_code):
         self.type(self.first_name_locator, name)
