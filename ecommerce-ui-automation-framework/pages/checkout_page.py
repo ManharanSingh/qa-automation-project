2c8810f4cb4777e419647cf4d5792a69a90984b9
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-18 14:43:41 +0530

    Update checkout_page.py

--- ecommerce-ui-automation-framework/pages/checkout_page.py
+++ ecommerce-ui-automation-framework/pages/checkout_page.py
@@ -21,6 +21,8 @@ class CheckoutPage(BasePage):
         self.type(self.last_name_locator, last_name)
         self.type(self.postal_code_locator, zip_code)
         self.click(self.continue_btn)
+
+        self.wait.until(visibility_of_element_located(self.finish_btn))
     
     def finish_order(self):
         self.click(self.finish_btn)
