69f0a084dfca93d93492e208631500322b263113
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-18 18:43:08 +0530

    Update checkout_page.py

--- ecommerce-ui-automation-framework/pages/checkout_page.py
+++ ecommerce-ui-automation-framework/pages/checkout_page.py
@@ -21,7 +21,8 @@ class CheckoutPage(BasePage):
         self.type(self.first_name_locator, name)
         self.type(self.last_name_locator, last_name)
         self.type(self.postal_code_locator, zip_code)
-        
+        self.wait.until(EC.text_to_be_present_in_element_value(self.postel_code_locator, zip_code))
+       
         self.click(self.continue_btn)
 
         self.wait.until(EC.visibility_of_element_located(self.finish_btn))
