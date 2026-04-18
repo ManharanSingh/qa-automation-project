810fc5dfa3f776ccfbb3297d1785f219d0363dbc
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-18 17:47:11 +0530

    Update base_page.py

--- ecommerce-ui-automation-framework/utils/base_page.py
+++ ecommerce-ui-automation-framework/utils/base_page.py
@@ -18,8 +18,11 @@ class BasePage:
         
     def type(self, locator, text):
         element = self.wait.until(EC.visibility_of_element_located(locator))
+        element.clear()
         element.send_keys(text)
 
+        self.wait.until(lambda d: element.get_attribute("value") == str(text))
+        
     def get_text(self, locator):
         return self.wait.until(EC.visibility_of_element_located(locator)).text
     
