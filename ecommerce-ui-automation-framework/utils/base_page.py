f496233c9eececcc5a46f6597b6814de1de0f859
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-22 19:02:02 +0530

    Update base_page.py

--- ecommerce-ui-automation-framework/utils/base_page.py
+++ ecommerce-ui-automation-framework/utils/base_page.py
@@ -21,7 +21,7 @@ class BasePage:
         
         
     def type(self, locator, text):
-        element = wait.until(EC.element_to_be_clickable(locator))
+        element = self.wait.until(EC.element_to_be_clickable(locator))
         element.click()
         element.clear()
         element.send_keys(text)
