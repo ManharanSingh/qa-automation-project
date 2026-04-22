f49eee00117ba981d97418493ddd64c91251429d
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-22 18:39:35 +0530

    Update conftest.py

--- ecommerce-ui-automation-framework/conftest.py
+++ ecommerce-ui-automation-framework/conftest.py
@@ -9,8 +9,6 @@ def driver_setup():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window_size=1920,1080")
-    options.add_argument("--disable-gpu")
     
     driver = webdriver.Chrome(options=options)
     driver.maximize_window()
