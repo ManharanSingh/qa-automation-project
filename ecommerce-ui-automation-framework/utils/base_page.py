e914f9dcdd81568abee3d4edf5b084a9958673a8
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-22 18:16:50 +0530

    Update base_page.py

--- ecommerce-ui-automation-framework/utils/base_page.py
+++ ecommerce-ui-automation-framework/utils/base_page.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+
 
 
 
@@ -22,7 +22,7 @@ class BasePage:
         
     def type(self, locator, text):
         element = self.wait.until(EC.element_to_be_clickable(locator))
-        self.driver.execute_script(""" arguments[0].value = arguments[1]; arguments[0].dispatchEvent(new Event('input',{bubbles:true}));""", element, text)
+        element.send_keys(text)
  
     def get_text(self, locator):
         return self.wait.until(EC.visibility_of_element_located(locator)).text
