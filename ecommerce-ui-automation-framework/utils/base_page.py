ebc12e068b2e53cbf90a20ecf0f675a105aae7e5
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-22 15:00:47 +0530

    Update base_page.py

--- ecommerce-ui-automation-framework/utils/base_page.py
+++ ecommerce-ui-automation-framework/utils/base_page.py
@@ -21,7 +21,7 @@ class BasePage:
         
         
     def type(self, locator, text):
-        element = self.wait.until(EC.visibility_of_element_located(locator))
+        element = self.wait.until(EC.element_to_be_clickable(locator))
         element.click()
         self.wait.until(lambda d, el=element: d.execute_script("return document.activeElement === arguments[0];", el))
         element.clear()
