385cc76897c59b6e1ac6be4d5e38a014cbe31a9d
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-22 15:38:22 +0530

    Update base_page.py

--- ecommerce-ui-automation-framework/utils/base_page.py
+++ ecommerce-ui-automation-framework/utils/base_page.py
@@ -22,10 +22,11 @@ class BasePage:
         
     def type(self, locator, text):
         element = self.wait.until(EC.element_to_be_clickable(locator))
-        self.driver.execute_script("arguments[0].value = '';", element)
-        self.driver.execute_script("arguments[0].click();", element)
+        element.clear()
+        element.click()
         element.send_keys(text)
-       
+
+        self.wait.until(lambda d: element.get_attribute("value") == value)
         
     def get_text(self, locator):
         return self.wait.until(EC.visibility_of_element_located(locator)).text
