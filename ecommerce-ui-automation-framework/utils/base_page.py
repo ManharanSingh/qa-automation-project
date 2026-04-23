990e0ce982ac2adf347217676c3468424f333722
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-23 14:41:11 +0530

    Update base_page.py

--- ecommerce-ui-automation-framework/utils/base_page.py
+++ ecommerce-ui-automation-framework/utils/base_page.py
@@ -21,11 +21,16 @@ class BasePage:
         
         
     def type(self, locator, text):
+        print("Before:", element.get_attribute("value"))
         element = self.wait.until(EC.element_to_be_clickable(locator))
         element.click()
         element = self.wait.until(EC.presence_of_element_located(locator))
         element.clear()
         element.send_keys(text)
+        
+        print("After:", element.get_attribute("value"))
+        time.sleep(2)
+        print("After 2 sec:", element.get_attribute("value"))
         
     def get_text(self, locator):
         return self.wait.until(EC.visibility_of_element_located(locator)).text
