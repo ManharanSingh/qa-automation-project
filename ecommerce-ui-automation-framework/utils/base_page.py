a1e7100f235dd1e4087cd779eb4ada1e094621b5
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-23 14:45:19 +0530

    Update base_page.py

--- ecommerce-ui-automation-framework/utils/base_page.py
+++ ecommerce-ui-automation-framework/utils/base_page.py
@@ -2,7 +2,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-
+   
 class BasePage:
 
     def __init__(self, driver):
