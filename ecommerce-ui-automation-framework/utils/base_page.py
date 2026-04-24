8c5def4f92bf027b3a83fce82c90193b62c05333
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-24 14:13:50 +0530

    Update base_page.py

--- ecommerce-ui-automation-framework/utils/base_page.py
+++ ecommerce-ui-automation-framework/utils/base_page.py
@@ -13,13 +13,29 @@ class BasePage:
         return self.wait.until(EC.visibility_of_element_located(locator))
         
     def click(self, locator):
-        element = self.wait.until(EC.element_to_be_clickable(locator))
-        
-        self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
-        self.driver.execute_script("arguments[0].click();", element)
+       element = self.wait.until(EC.element_to_be_clickable(locator))
+       self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
+       self.driver.execute_script("arguments[0].click();", element)
         
         
     def type(self, locator, text):
+       element = self.wait.until(EC.element_to_be_clickable(locator))
+       element.click()
+       active = driver.switch_to.active_element
+       if active != element:
+          raise ValueError("focus issue")
+
+       element.send_keys(text)
+       value_after = element.get_attribute("value")
+       if value_after != text:
+          raise ValueError(f"value after typing:{value_after}")
+
+       import time
+       time.sleep(1)
+
+       value_later = element.get_attribute("value")
+       if value_later != text:
+          raise ValueError("value after 1s:", value_later)
        
        
        
