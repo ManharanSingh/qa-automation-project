a0b8eb22ef5ecdcd4d62f75f360f5862d237cd5e
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-24 16:08:41 +0530

    Import By class from selenium.webdriver.common

--- ecommerce-ui-automation-framework/utils/base_page.py
+++ ecommerce-ui-automation-framework/utils/base_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import time
 from utils.logger import get_logger
 
