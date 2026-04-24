22fcb884c5c43727058b1e91739bdb6d8cabff00
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-24 17:40:10 +0530

    Update base_page.py

--- ecommerce-ui-automation-framework/utils/base_page.py
+++ ecommerce-ui-automation-framework/utils/base_page.py
@@ -73,7 +73,7 @@ class BasePage:
        if value_after != text:
           raise ValueError(f"value after typing:{value_after}")
 
-       
+           
        time.sleep(1)
 
        value_later = element.get_attribute("value")
