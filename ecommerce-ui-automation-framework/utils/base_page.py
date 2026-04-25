c024e4fb6249ed477d63217ecb6344a8e50e0769
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-25 06:35:11 +0530

    Refactor type method to simplify input handling
    Removed unnecessary logging and sleep statements from the type method.

--- ecommerce-ui-automation-framework/utils/base_page.py
+++ ecommerce-ui-automation-framework/utils/base_page.py
@@ -1,11 +1,11 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import time
-from selenium.webdriver.common.keys import Keys
-from utils.logger import get_logger
+
+
+
              
-logger = get_logger(__name__)
+
          
 class BasePage:
 
@@ -28,8 +28,6 @@ class BasePage:
        
     def type(self, locator, text):
        element = self.wait.until(EC.element_to_be_clickable(locator))
-       elements = self.driver.find_elements(*locator)
-       logger.info(f"element count:{len(elements)}")     
        element.click()
        self.driver.execute_script("arguments[0].focus();", element)    
        self.driver.execute_script(""" 
@@ -45,45 +43,6 @@ class BasePage:
           }
        input.dispatchEvent(event);
        """, element, text)
-         
-       
-       active = self.driver.switch_to.active_element
-       if active != element:
-          raise ValueError("focus issue")
-       logger.info(f"before : {element}")
-       logger.info(f"Tag:{element.tag_name}")
-       logger.info(f"Type:{element.get_attribute('type')}")
-       logger.info(f"verify 1: {element.is_displayed()}")
-       logger.info(f"verify 2: {element.is_enabled()}")
-
-       rect = element.rect
-       logger.info(f"verify 3:{rect}")                 
-       self.driver.execute_script("arguments[0].value = arguments[1];", element, text)
-       element = self.driver.find_element(*locator)
-       logger.info(f"new element:{element.get_attribute('value')}")
-         
-          
-       logger.info(f"immediate : {element.get_attribute('value')}")
-       time.sleep(0.5)
-       logger.info(f"After 0.5s:{element.get_attribute('value')}")
-       new_element = self.driver.find_element(*locator)
-       logger.info(f"after : {new_element}")
-       logger.info(f"same element? :{element == new_element}")
-       value_after = element.get_attribute("value")
-       if value_after != text:
-          raise ValueError(f"value after typing:{value_after}")
-
-           
-       time.sleep(1)
-
-       value_later = element.get_attribute("value")
-       if value_later != text:
-          raise ValueError("value after 1s:", value_later)
-       
-       return element
-       
-       
-       
        
     def get_text(self, locator):
         return self.wait.until(EC.visibility_of_element_located(locator)).text
