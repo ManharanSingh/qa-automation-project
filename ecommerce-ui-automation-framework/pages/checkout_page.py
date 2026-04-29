ed7dd8d0e4758efe4fd88589dbab028f42c992c6
ManharanSingh <manharanmaravi332@gmail.com>
2026-04-30 01:16:34 +0530

    Refactor fill_details method to use checkout_type

--- ecommerce-ui-automation-framework/pages/checkout_page.py
+++ ecommerce-ui-automation-framework/pages/checkout_page.py
@@ -17,9 +17,9 @@ class CheckoutPage(BasePage):
         self.click(self.checkout_btn)
 
     def fill_details(self, name, last_name, zip_code):
-            self.type(self.first_name_locator, name)
-            self.type(self.last_name_locator, last_name)
-            self.type(self.postal_code_locator, zip_code)
+            self.checkout_type(self.first_name_locator, name)
+            self.checkout_type(self.last_name_locator, last_name)
+            self.checkout_type(self.postal_code_locator, zip_code)
 
             self.click(self.continue_btn)
     
