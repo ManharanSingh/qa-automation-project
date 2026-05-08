c72057f7a86e03ac6fc3b05611dfe6a4ecba17c6
ManharanSingh <manharanmaravi332@gmail.com>
2026-05-08 19:25:55 +0530

    Update schemas.py

--- API_Automation_Framework_(E-commerce)/data/schemas.py
+++ API_Automation_Framework_(E-commerce)/data/schemas.py
@@ -1,55 +1,67 @@
 product_schema = {
-  "type": "object",
-  "properties": {
-    "id": {
-      "type": "integer",
-      "minimum": 1
-    },
-    "title": {
-      "type": "string",
-      "minLength": 1
-    },
-    "price": {
-      "type": "number",
-      "exclusiveMinimum": 0
-    },
-    "description": {
-      "type": "string",
-      "minLength": 1
-    },
-    "category": {
-      "type": "string",
-      "minLength": 1
-    },
-    "image": {
-      "type": "string",
-      "format": "uri"
-    },
-    "rating": {
-      "oneOf": [
-        {
-          "type": "object",
-          "properties": {
-            "rate": { "type": "number" },
-            "count": { "type": "integer" }
-          },
-          "required": ["rate", "count"],
-          "additionalProperties": False
-        },
-        {
-          "type": "number"
-        }
-      ]
-    }
-  },
-  "required": [
-    "id",
-    "title",
-    "price",
-    "description",
-    "category",
-    "image",
-    "rating"
-  ]
-}
+    "type": "object",
 
+    "properties": {
+        "id": {
+            "type": "integer",
+            "minimum": 1
+        },
+
+        "title": {
+            "type": "string",
+            "minLength": 1
+        },
+
+        "price": {
+            "type": "number",
+            "exclusiveMinimum": 0
+        },
+
+        "description": {
+            "type": "string",
+            "minLength": 1
+        },
+
+        "category": {
+            "type": "string",
+            "minLength": 1
+        },
+
+        "rating": {
+            "oneOf": [
+                {
+                    "type": "object",
+                    "properties": {
+                        "rate": {"type": "number"},
+                        "count": {"type": "integer"}
+                    },
+                    "required": ["rate", "count"]
+                },
+                {
+                    "type": "number"
+                }
+            ]
+        },
+
+        "image": {
+            "type": "string"
+        },
+
+        "images": {
+            "type": "array"
+        },
+
+        "thumbnail": {
+            "type": "string"
+        }
+    },
+
+    "required": [
+        "id",
+        "title",
+        "price",
+        "description",
+        "category",
+        "rating"
+    ]
+}
